4b004cfc677460c0a7b8b0b7ef113ef6ba7f3721
Nikita Masev <n.masev@bastion-tech.ru>
2023-09-12 12:15:31 +0300

    updated export file;

--- lib/iot_client_starter.dart
+++ lib/iot_client_starter.dart
@@ -6,6 +6,7 @@ export 'di/config_domains.dart';
 export 'di/config_sources.dart';
 export 'domain/auth/auth_bloc.dart';
 export 'domain/iot_devices/iot_devices_bloc.dart';
+export 'internal/errors/common_errors.dart';
 export 'models/channel_state.dart';
 export 'services/iot_communicator/iot_communicator_service.dart';
 export 'services/iot_connector/channel_state_watcher.dart';
